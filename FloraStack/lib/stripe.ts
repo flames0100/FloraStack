9a64c7bfddda7734d6215f0eedfd5b7cc389b948
flames0100 <87822207+flames0100@users.noreply.github.com>
2025-10-08 16:10:17 +0100

    Delete FloraStack directory

--- FloraStack/lib/stripe.ts
+++
@@ -1,6 +0,0 @@
-import Stripe from "stripe"
-
-export const stripe = new Stripe(process.env.STRIPE_API_KEY!, { //its saying like || '' instead we can use !
-  apiVersion: "2022-11-15",
-  typescript: true,
-});
